aedf3d40976cf2ddadf23c812d244beb93be7a3b
sac1010 <sachingirish101@gmail.com>
2024-03-02 21:14:30 +0530

    fixes

--- src/components/GameComplete.tsx
+++ src/components/GameComplete.tsx
@@ -3,6 +3,7 @@ import earned from "../images/earned.svg";
 import monkey from "../images/monkey.svg";
 import banana from "../images/banana.svg";
 import nextButton from "../images/nextButton.svg";
+import Loader from "./Loader";
 
 type Props = { setScreen: any };
 
@@ -19,7 +20,8 @@ const bananaPositions = [
 const GameComplete = ({ setScreen }: Props) => {
   return (
     <div className="w-full h-full flex items-center justify-center overflow-hidden">
-        <div className="absolute inset-0 bg-black opacity-60"></div>
+      <div className="absolute inset-0 bg-black opacity-60"></div>
+      <Loader bananas={6} />
       <div className="relative">
         <img className="w-[500px]" src={earned} alt="" />
         <img
--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -9,7 +9,7 @@ const Loader = ({ bananas }: Props) => {
   const width = ((bananas + 1) / 7) * 100;
 
   return (
-    <div className="w-[400px] transform transition h-6 fixed top-10 left-0 right-0 mx-auto rounded-full bg-gray-50 ">
+    <div className="w-[400px] z-10 transform transition h-6 fixed top-10 left-0 right-0 mx-auto rounded-full bg-gray-50 ">
       <div style={{ width: `${width}%`, height: "100%" }}>
         <img
           className="w-full h-full object-cover rounded-full"
